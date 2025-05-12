f08d420b34a306176adee42871cc36587bb82e57
Carlos Mario Hernández León <84859398+CharloHdz@users.noreply.github.com>
2025-05-12 11:34:44 -0600

    Port android

--- Assets/Scripts/Niveles/Nivel2/S_Nivel2.cs
+++ Assets/Scripts/Niveles/Nivel2/S_Nivel2.cs
@@ -5,6 +5,9 @@ public class S_Nivel2 : MonoBehaviour
     public GameObject[] Blocks;
     public DeleteArea_UI deleteArea;
     public Canvas canvas;
+
+    [Header("Capas")]
+    public GameObject[] layers;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,13 +23,13 @@ public class S_Nivel2 : MonoBehaviour
     public void SpawnActionBlock1(){
         //Spawn como hijo de canvas y dentro de deleteArea
         GameObject block = Instantiate(Blocks[1], transform.position, Quaternion.identity);
-        block.transform.SetParent(canvas.transform, false);
+        block.transform.SetParent(layers[1].transform, false);
         block.transform.position = deleteArea.rectTransform.position;
     }
 
     public void SpawnInputBlock(){
         GameObject block = Instantiate(Blocks[0], transform.position, Quaternion.identity);
-        block.transform.SetParent(canvas.transform, false);
+        block.transform.SetParent(layers[0].transform, false);
         block.transform.position = deleteArea.rectTransform.position;
     }
 }
